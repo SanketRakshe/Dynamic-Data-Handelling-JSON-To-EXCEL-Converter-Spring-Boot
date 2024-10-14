6f206d938ba29d5f9f323613e3962f44f75f8d6c
SanketRakshe <sanketrakshe07@gmail.com>
2024-10-14 14:28:48 +0530

    Initial setup done moving forward

---
+++ src/main/java/com/example/DataConversion2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataConversion2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataConversion2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DataConversion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataConversion2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
