fe5979428db5f8104097781304df53a7fccd8ab6
SanketRakshe <sanketrakshe07@gmail.com>
2024-10-14 14:41:59 +0530

    File Service Updated

---
+++ src/main/java/com/example/service/FileService.java
@@ -0,0 +1,42 @@
+package com.example.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.model.PersonData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class FileService {
+	
+	private final ObjectMapper objectMapper;
+    private final ExcelGenerator excelGenerator;
+
+    public FileService(ObjectMapper objectMapper, ExcelGenerator excelGenerator) {
+        this.objectMapper = objectMapper;
+        this.excelGenerator = excelGenerator;
+    }
+
+    public void generateExcelFromJson(String jsonFolderPath, String outputExcelPath) throws IOException {
+        List<PersonData> personDataList = new ArrayList<>();
+        
+        File folder = new File(jsonFolderPath);
+        File[] jsonFiles = folder.listFiles();
+
+        if (jsonFiles != null) {
+            for (File jsonFile : jsonFiles) {
+                if (jsonFile.isFile() && jsonFile.getName().endsWith(".json")) {
+                    PersonData personData = objectMapper.readValue(jsonFile, PersonData.class);
+                    personDataList.add(personData);
+                }
+            }
+        }
+        
+        // Call to generate Excel file
+        excelGenerator.generateExcel(personDataList, outputExcelPath);
+    }
+}
---
+++ src/main/java/com/example/utils/ExcelGenerator.java
@@ -0,0 +1,17 @@
+package com.example.utils;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Component;
+
+import com.example.model.PersonData;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class ExcelGenerator {
+
+    
+}
